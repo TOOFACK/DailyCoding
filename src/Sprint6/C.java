1db6ca84ff53e499eb26b275d4babd72d4914007
TOOFACK <paulrussianvodka@gmail.com>
2020-11-24 15:31:58 +0300

    Time Complexity O(N*M) N - amount unique of chars in line and M - amount of i - char in line.

--- src/Sprint6/C.java
+++ src/Sprint6/C.java
@@ -4,7 +4,7 @@ package Sprint6;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
+import java.util.*;
 
 public class C {
 
@@ -20,9 +20,64 @@ public class C {
                 vals.put(line.charAt(i), 1);
             } else {
                 int t = vals.get(line.charAt(i));
-
+                t++;
+                vals.put(line.charAt(i), t);
             }
         }
+
+        Set<Character> keys2 = vals.keySet();
+        ArrayList<Character> keys = new ArrayList<>(keys2);
+        Collections.sort(keys);
+
+
+
+
+
+
+        StringBuffer ans = new StringBuffer();
+        boolean have1 = false;
+        String haveOne = "";
+        int len = 0;
+
+        for(Character c : keys){
+            int am = vals.get(c);
+            if(vals.get(c) % 2 == 0){
+                while(am > 0){
+                    ans.insert(len,c);
+                    ans.insert(ans.length() - len,c);
+                    len++;
+                    am = am - 2;
+
+
+                }
+            } else{
+
+                while(am > 0){
+                    if(am == 1 && have1){
+                        break;
+                    }
+                    if (am == 1) {
+                        have1 = true;
+                        haveOne = String.valueOf(c);
+                        break;
+                    }
+
+                    ans.insert(len,c);
+                    ans.insert(ans.length() - len, c);
+                    len++;
+                    am = am - 2;
+
+
+                }
+            }
+
+
+        }
+
+        if(have1){
+            ans.insert(ans.length() /2, haveOne);
+        }
+        System.out.println(ans);
     }
 
     }
